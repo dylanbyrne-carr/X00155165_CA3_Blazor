8558d7b9428ed8b2586688396195ed9213351b59
dylanbeforechrist <dylanbyrnecarr@gmail.com>
2025-11-25 22:56:58 +0000

    Attempt probably 8: Think were making progress

--- Pages/Home.razor.cs
+++ Pages/Home.razor.cs
@@ -81,7 +81,7 @@ public partial class Home
 
         if (raceSession != null)
         {
-            Navigation.NavigateTo($"/race/{raceSession.SessionKey}");
+            Navigation.NavigateTo($"race/{raceSession.SessionKey}");
         }
         else
         {
